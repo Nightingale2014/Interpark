387382fb4b983b4da68e05656919180bc59e4eff
Nightingale2014 <topoftoppark@naver.com>
2025-04-07 01:47:30 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // tailwind 스타일링이 적용된 index.css
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
